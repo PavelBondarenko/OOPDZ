537e73cd769f426414f42e9b5c356b8362c5fbfb
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-07-10 20:38:28 +0300

    update

--- logform/install/logbd.sql
+++ logform/install/logbd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июл 07 2020 г., 20:04
+-- Время создания: Июл 10 2020 г., 20:37
 -- Версия сервера: 5.5.67-MariaDB
 -- Версия PHP: 7.4.5
 
@@ -26,41 +26,87 @@ USE `logbd`;
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `form`
+-- Структура таблицы `group`
 --
 
-CREATE TABLE `form` (
+CREATE TABLE `group` (
   `id` int(11) NOT NULL,
-  `login` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kod` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `form`
+-- Дамп данных таблицы `group`
 --
 
-INSERT INTO `form` (`id`, `login`, `password`) VALUES
-(1, 'pavel', '1234');
+INSERT INTO `group` (`id`, `name`, `kod`) VALUES
+(1, 'Администратор', 'admin'),
+(2, 'Пользователь', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `group_id`) VALUES
+(2, 'Pavel', '1234', 1),
+(3, 'Vasya', '123', 2);
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `form`
+-- Индексы таблицы `group`
 --
-ALTER TABLE `form`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `group_id` (`group_id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT для таблицы `form`
+-- AUTO_INCREMENT для таблицы `group`
 --
-ALTER TABLE `form`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
